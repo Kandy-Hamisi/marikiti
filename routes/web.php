3249a07504e8b9308b06a378474dd278fc5bfe9d
Kevin Kipkkoech <Kevin Kipkkoech>
2017-07-12 23:10:16 +0300

    Setting things up

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', 'HomeController@index')->name('home');
+
+Route::get('shirts', 'HomeController@shirts')->name('shirts');
+
+Route::get('shirt', 'HomeController@shirt')->name('shirt');
