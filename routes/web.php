6f773a4e54588de2c5c7a64a3a47790fcf6517f8
Kevin Kipkkoech <Kevin Kipkkoech>
2017-07-13 19:03:56 +0300

    Adding products

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
     Route::get('/', function(){
         return view('admin.index');
     })->name('admin.index');
+
+    Route::resource('product', 'ProductsController');
+    Route::resource('product', 'ProductsController');
 });
